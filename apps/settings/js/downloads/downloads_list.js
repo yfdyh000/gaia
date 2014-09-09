0c76b1cf4fbee706468d8c6d1a9eeafa056cd829
YFdyh000 <yfdyh000@gmail.com>
2014-09-09 21:24:44 +0800

    Refine the 'true' to true.

--- apps/settings/js/downloads/downloads_list.js
+++ apps/settings/js/downloads/downloads_list.js
@@ -43,7 +43,7 @@
       emptyDownloadsContainer.hidden = true;
     }
 
-    editButton.disabled = isEmpty ? 'true' : '';
+    editButton.disabled = isEmpty ? true : false;
   }
 
   function _newDownload(download) {
